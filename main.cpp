e0047db754495a65ac2752aab5e488dba1735e57
emould2707 <emould@students.avc.edu>
2026-03-31 19:29:22 -0700

    a57 done

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 int main()
 {
-	int n1, n2;
-	int N, sum;
+	int n1, n2, N, sum;
 
 	cin >> n1 >> n2;
 	cin >> N;
-
+	cout << n1 << " " << n2; 
 	// TODO
+	for(int i =2; i < N; i++){
+		sum = n1 + n2;
+		cout << " " << sum;
+		n1 = n2;
+		n2 = sum;
+	}
+	return 0;
 	// TODO
 }
